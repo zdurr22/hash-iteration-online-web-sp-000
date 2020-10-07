f27162e2df4bd8c5fd42ddefbf51b90a2139caad
Zachary Durr <zdurr22@gmail.com>
2020-10-07 01:58:15 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
   passengers.select do |suite, name|
-    if name.start_with?("a") && suite == :suite_a
+    if suite == :suite_a && name.start_with?("a")
       winner = name
       puts winner
     end
